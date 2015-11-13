d69901808652d550888f0022e7cb1e082a26569f
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-13 13:31:05 -0800

    added bower rc file

---
+++ boardApp.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var PORT = process.env.PORT || 3000;
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/boardapp");
+
+var app = express();
+
+
+app.set('view engine','jade');
+
+// GENERAL MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// ROUTES
+//redirects to boards
+app.use('/', require('./routes'));
+//channels all user info and messages to the same place
+app.use('/boards', require('./routes/boards'));
+app.use("/boards", require("./routes/boards"))
+app.use("/delete", require("./routes/boards"))
+
+// 404 HANDLER
+app.use(function(req, res){
+  res.status(404).render('404')
+})
+
+app.listen(PORT, function(){
+  console.log('Listening on port ', PORT);
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 can add new items and store them. I can pass an id in for deleting but wasnt able to get the item actually select and delete. 
+
+heroku: https://intense-forest-9779.herokuapp.com/boards
